e44b040ab2e18d43087cfb8b69364c88a1e96dc5
isha chopde <isha.chopde250794@gmail.com>
2017-10-03 08:04:42 -0700

    Committed other html and css files

--- server.js
+++ server.js
@@ -1,15 +1,47 @@
 const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
 
-const host = '127.0.0.1';
-const port = 3000;
+const mimeTypes = {
+	"html": "text/html",
+	"jpeg": "image/jpeg",
+	"jpg": "image/jpg",
+	"png": "image/png",
+	"js": "text/javascript",
+	"css": "text/css"
+};
 
-const server = http.createServer((request, response) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type','text/plain');
-    response.end('Hello\n');
+http.createServer(function(req, res){
+	var uri = url.parse(req.url).pathname;
+	var fileName = path.join(process.cwd(), unescape(uri));
+	console.log('Loading '+ uri);
+	var stats;
 
-});
+	try{
+		stats = fs.lstatSync(fileName);
+	} catch(e){
+		res.writeHead(404, {'Content-type': 'text/plain'});
+		res.write('404 Not Found\n');
+		res.end();
+		return;
+	}
 
-server.listen(port, host, () => {
-   console.log(`Server running at http://${host}:${port}/`);
-});
+	if(stats.isFile()){
+		var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+		res.writeHead(200, {'Content-type': mimeType});
+
+		var fileStream = fs.createReadStream(fileName);
+		fileStream.pipe(res);
+	} else if(stats.isDirectory()) {
+		res.writeHead(302, {
+			'Location': 'index.html'
+		});
+		res.end();
+	} else {
+		res.writeHead(500, {'Content-type':'text/plain'});
+		res.write('500 Internal Error\n');
+		res.end();
+	}
+
+}).listen(1337);
